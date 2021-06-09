d5829992ea8a67938bccfb39f59e01c510f53d4b
HannahJT <84517064+Hannah-Jurewicz-Turner@users.noreply.github.com>
2021-06-09 13:13:24 +0000

    added a space to java.

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// 
+// connects the button to the funtion below.
 document.getElementById('button').addEventListener('click', checkAge)
 let userInput = 0
 
 function checkAge () {
-userInput = document.getElementById('input').value
-userInput = parseInt(userInput)
-
-// 
-if (userInput >= 17) {
+  userInput = document.getElementById('input').value
+  userInput = parseInt(userInput)
+  
+  // Compares the age of the user to the ages allowed to watch specific movies.
+  if (userInput >= 17) {
   document.getElementById('answer').innerHTML = 'You can watch an R rated movie alone.'
-} else if (userInput >= 13) {
-  document.getElementById('answer').innerHTML = 'You can watch a PG-13 rated movie alone.'
-} else if (userInput >= 5) {
-  document.getElementById('answer').innerHTML = 'You can watch a G or PG rated movie alone.'
-} else {
-  document.getElementById('answer').innerHTML = 'Um. You are too young to watch anything.'
-}
+  } else if (userInput >= 13) {
+    document.getElementById('answer').innerHTML = 'You can watch a PG-13 rated movie alone.'
+    } else if (userInput >= 5) {
+      document.getElementById('answer').innerHTML = 'You can watch a G or PG rated movie alone.'
+      } else {
+        document.getElementById('answer').innerHTML = 'Um. You are too young to watch anything.'
+        }
 }
